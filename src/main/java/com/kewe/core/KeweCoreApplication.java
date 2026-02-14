4eda9d6c98c48c6059a0c45e21fd961770c87581
stwalke2 <stwalke2@gmail.com>
2026-02-14 17:20:00 -0500

    Initial Spring Boot scaffold

---
+++ src/main/java/com/kewe/core/KeweCoreApplication.java
@@ -0,0 +1,13 @@
+package com.kewe.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeweCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeweCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kewe/core/KeweCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kewe.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeweCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
